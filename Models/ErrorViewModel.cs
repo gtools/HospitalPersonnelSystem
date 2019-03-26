fe92fc05d83957a4bdf190b2966efc94f6552be9
GTool <gtool@vip.qq.com>
2019-03-26 14:51:40 +0800

    第一次提交

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HospitalPersonnelSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
